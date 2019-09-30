b7b9c18f21a1d57b9dd251fad7ed62a69c88f30a
Jigar A. (Odoo) <jam@odoo.com>
2019-09-30 07:59:51 -0700

    [ADD] keyboard listner with product search

--- iot_oxp/static/src/js/models.js
+++ iot_oxp/static/src/js/models.js
@@ -1,25 +1,51 @@
 odoo.define('iot_oxp.models', function (require){
-"use strict";
+'use strict';
 
 var models = require('point_of_sale.models');
 var DeviceProxy = require('iot.widgets').DeviceProxy;
 
-
 models.load_models({
     model: 'iot.device',
     fields: ['iot_ip', 'iot_id', 'identifier', 'type'],
-    domain: function(self){
-        return [['id', '=', self.config.iface_keyboard_id[0]]]
+    domain: function (self) {
+        return [['id', '=', self.config.iface_keyboard_id[0]]];
     },
-    loaded: function(self, iot_devices) {
-        if (iot_devices){
+    loaded: function (self, iot_devices) {
+        if (iot_devices) {
             var iot_device = iot_devices[0];
-            self.keybaord = new DeviceProxy({
+            self.keyboard = new DeviceProxy({
                 iot_ip: iot_device.iot_ip,
                 identifier: iot_device.identifier,
             });
         }
     },
+});
+
+var posmodel_super = models.PosModel.prototype;
+models.PosModel = models.PosModel.extend({
+    after_load_server_data: function () {
+        var self = this;
+        return posmodel_super.after_load_server_data.apply(this, arguments)
+            .then(function () {
+                self.keyboard.add_listener(self._onKeyPressed.bind(self));
+            });
+    },
+
+    _onKeyPressed: function (data) {
+        console.log('Key Pressed : ' + data.value);
+        var input = $('.searchbox input')[0];
+        if (input) {
+            input.dispatchEvent(new KeyboardEvent('keypress', {char: data.value}));
+            if (data.value == 'BACKSPACE'){
+                input.value = input.value.slice(0, -1);
+            }else if (data.value == 'SPACE'){
+                input.value +=  ' ';
+            }else{
+                input.value += data.value;
+            }
+            input.dispatchEvent(new KeyboardEvent('keyup', {char: data.value}));
+        }
+    }
 });
 
 });
