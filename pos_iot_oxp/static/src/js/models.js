08fc275ab2a0465c2be6bad6bda0b75b465cfaec
gab-odoo <gab@odoo.com>
2019-09-30 17:00:27 +0200

    Update models.js

--- pos_iot_oxp/static/src/js/models.js
+++ pos_iot_oxp/static/src/js/models.js
@@ -5,28 +5,24 @@ var models = require('point_of_sale.models');
 var DeviceProxy = require('iot.widgets').DeviceProxy;
 
 models.load_models({
-   model: 'iot_device',
-   fields: ['iot_ip', 'identifier', 'type'],
-   domain: function (self) {
-       return [['id', '=', self.config.iot_keyboard_id[0]]]
-   },
-   loaded: function (self, iot_devices) {
+    model: 'iot.device',
+    fields: ['iot_ip', 'identifier', 'type'],
+    domain: function(self) {
+        return [['id', '=', self.config.iot_keyboard_id[0]]];
+    },
+    loaded: function(self, iot_devices) {
         if (iot_devices) {
             var iot_device = iot_devices[0];
-            self.keyboard = new DeviceProxy({
-                iot_ip: iot_device.iot_ip,
-                identifier: iot_device.identifier,
-            });
+            self.iot_device_proxies[iot_device.type] = new DeviceProxy({ iot_ip: iot_device.iot_ip, identifier: iot_device.identifier });
         }
-   },
- });
+    },
+});
 
 var posmodel_super = models.PosModel.prototype;
 models.PosModel = models.PosModel.extend({
     after_load_server_data: function () {
         var self = this;
-        return posmodel_super.after_load_server_data.apply(this, arguments)
-            .then(function() {
+        return posmodel_super.after_load_server_data.apply(this, arguments).then(function () {
             self.iot_device_proxies.keyboard.add_listener(self._onKeyPressed.bind(self));
         });
     },
@@ -43,5 +39,5 @@ models.PosModel = models.PosModel.extend({
             input.dispatchEvent(new KeyboardEvent("keyup", {char : data.value}));
         }
     }
-    });
+});
 });
