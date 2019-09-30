ce369356572d20e57c7607030b00f63a8e4ed5bb
= <=>
2019-09-30 12:24:12 +0200

    Created OXPKeybosrDriver.py

---
+++ OXPKeyboardDriver.py
@@ -0,0 +1,16 @@
+from odoo.addons.hw_drivers.controllers.driver import Driver
+
+class OXPkeyboardDriver(Driver):
+  connection type = 'usb'
+
+  def __init__(self, device):
+    super(OXPKeyboardDriver, self),__init__(device)
+    self._device_type = "keyboard"
+    self._device_connection = "direct"
+    self._device_name = "USB Keyboard"
+
+  @classmethod
+  def supported(cls, device):
+    for cfg in device:
+      for itf in cfg:
+        return itf.bInterfaceClass == 3 and itf.InterfaceProtocol == 1
---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+  'name': 'pos_iot_oxp',
+  'version': '1.0',
+  'category': 'Point of Sale',
+  'sequence': 6,
+  'summary': '',
+  'description': '',
+  'depends': ['pos_iot'],
+  'installable': True,
+}
+
