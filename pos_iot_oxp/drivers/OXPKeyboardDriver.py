72a6792e5283f4c00b3aa8e898b187a24b475617
Mikul <mikulbuncha@Mikuls-MacBook-Pro.local>
2019-09-30 12:26:52 +0200

    Copy paste ninja

---
+++ pos_iot_oxp/drivers/OXPKeyboardDriver.py
@@ -0,0 +1,17 @@
+
+from odoo.addons.hw_drivers.controllers.driver import Driver
+
+class OXPKeyboardDriver(Driver):
+    connection_type = 'usb'
+
+    def __init__(self, device):
+        super(OXPKeyboardDriver, self).__init__(device)
+        self._device_type = "keyboard"
+        self._device_connection = "direct"
+        self._device_name = "USB Keyboard"
+
+    @classmethod
+    def supported(cls, device):
+        for cfg in device:
+            for itf in cfg:
+                return itf.bInterfaceClass == 3 and itf.bInterfaceProtocol == 1
