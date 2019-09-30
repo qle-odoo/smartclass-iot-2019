6f6888209bb399086f88403a6c601b6955cb21b9
Jigar A. (Odoo) <jam@odoo.com>
2019-09-30 03:40:59 -0700

    [IMP]

--- iot_oxp/drivers/OXPKeyboardDrivers.py
+++ iot_oxp/drivers/OXPKeyboardDrivers.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pprint
-
-from odoo.addons.hw_drivers.controllers.driver import Driver, event_manager
-
+from odoo.addons.hw_drivers.controllers.driver import Driver
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -13,19 +11,14 @@ class OXPKeyboardDriver(Driver):
 
     def __init__(self, device):
         super(OXPKeyboardDriver, self).__init__(device)
-        self._device_type = 'keyboard'
-        self._device_connection = 'direct' #(network or direct here ) changes the device icon show in IOT module. (direct when corrected to IOT, and network when on LAN device)
+        self._device_type = 'keyboard' 
+        self._device_connection = 'direct' # possible value : direct - network - bluetooth - serial - hdmi ; changes the icon in ui, new type need to be handled client side
         self._device_name = 'USB Keyboard'
+
 
     @classmethod
     def supported(cls, device):
-        '''
-        Method to Check if device is supported or not 
-        device will be have list of devices using python lisb
-        i.e. USb loop gives all USB connected 
-
-        return bool: if supported True else False
-        '''
-        pp.pprint(device)
         for cfg in device:
             for itf in cfg:
+                return itf.bInterfaceClass == 3 and itf.bInterfaceProtocol == 1:
+        return False
