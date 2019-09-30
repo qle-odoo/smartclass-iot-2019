bd524b13a9d0f29f3f41d7991efd3aaa52b28faa
gab-odoo <gab@odoo.com>
2019-09-30 14:51:34 +0200

    [IMP] pos_iot_oxp: add assets and js event

---
+++ pos_iot_oxp/static/js/models.js
@@ -0,0 +1,23 @@
+odoo.define('pos_iot_oxp.models', function (require) {
+"use strict";
+
+var models = require('point_of_sale.models');
+
+models.load_models({
+   model: 'iot_device',
+   fields: ['iot_ip', 'identifier', 'type'],
+   domain: function (self) {
+       return [['id', '=', self.config.iot_keyboard_id[0]]]
+   },
+   loaded: function (self, iot_devices) {
+        if (iot_devices) {
+            var iot_device = iot_devices[0];
+            self.keyboard = new DeviceProxy({
+                iot_ip: iot_device.identifier,
+                identifier: iot_device.identifier,
+            });
+        }
+   },
+ });
+
+});
