8d76e1f98beae722f74ccaafeb830764262b7e0a
Jigar A. (Odoo) <jam@odoo.com>
2019-09-30 06:59:34 -0700

    [ADD] event listner

--- iot_oxp/drivers/OXPKeyboardDrivers.py
+++ iot_oxp/drivers/OXPKeyboardDrivers.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import pprint
-from odoo.addons.hw_drivers.controllers.driver import Driver
+import evdev
+
+from odoo.addons.hw_drivers.controllers.driver import Driver, event_manager
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -20,6 +22,7 @@ class OXPKeyboardDriver(Driver):
         self._device_type = 'keyboard' 
         self._device_connection = 'direct' 
         self._device_name = 'USB Keyboard'
+        self._input_device = self._get_evdev_device()
 
 
     @classmethod
@@ -30,5 +33,19 @@ class OXPKeyboardDriver(Driver):
         '''
         for cfg in device:
             for itf in cfg:
-                return itf.bInterfaceClass == 3 and itf.bInterfaceProtocol == 1:
+                return itf.bInterfaceClass == 3 and itf.bInterfaceProtocol == 1
         return False
+
+    def _get_evdev_device(self):
+        for path in reversed(evdev.list_devices()):
+            device = evdev.InputDevice(path)
+            if self.dev.idVendor == device.info.vendor and self.dev.idProduct == device.info.product:
+                return device
+
+    def run(self):
+        for event in self._input_device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY:
+                data = evdev.categorize(event)
+                if data.keystate:
+                    self.data['value'] = data.keycode.replace('KEY_', '')
+                    event_manager.device_changed(self)
