f7047f976ba56320fdb72ce2c92e751338c90d5f
= <=>
2019-09-30 13:37:17 +0200

    Coppied from master solution

--- OXP_IoT_Driver/drivers/OXPKeyboardDriver.py
+++ OXP_IoT_Driver/drivers/OXPKeyboardDriver.py
@@ -1,16 +1,20 @@
-from odoo.addons.hw_drivers.controllers.driver import Driver
+from odoo.addons.hw_drivers.controllers.driver import Driver, event_manager
 
-class OXPkeyboardDriver(Driver):
-  connection type = 'usb'
+class OXPKeyboardDriver(Driver):
+    connection_type = 'usb'
 
-  def __init__(self, device):
-    super(OXPKeyboardDriver, self).__init__(device)
-    self._device_type = "keyboard"
-    self._device_connection = "direct"
-    self._device_name = "USB Keyboard"
+    def __init__(self, device):
+        super(OXPKeyboardDriver, self).__init__(device)
+        self._device_type = 'keyboard'
+        self._device_connection = 'direct'
+        self._device_name = self._get_keyboard_name()
+        self._input_device = self._get_evdev_device()
 
-  @classmethod
-  def supported(cls, device):
-    for cfg in device:
-      for itf in cfg:
-        return itf.bInterfaceClass == 3 and itf.bInterfaceProtocol == 1
+    @classmethod
+    def supported(cls, device):
+        for cfg in device:
+            for itf in cfg:
+                if itf.bInterfaceClass == 3 and itf.bInterfaceProtocol == 1:
+                    return True
+        return False
+
