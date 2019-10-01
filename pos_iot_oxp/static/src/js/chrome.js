fdeaa61aa1dd4b629419b12088fb9994d1c99a3d
Antoine Prieels <anp@odoo.com>
2019-10-01 09:23:28 +0200

    Change LED status

---
+++ pos_iot_oxp/static/src/js/chrome.js
@@ -0,0 +1,28 @@
+odoo.define('pos_iot_oxp.chrome', function (require) {
+"use.strict";
+
+var chrome = require('point_of_sale.chrome');
+
+chrome.Chrome.include({
+    keyboard_button_widget: {
+        'name': 'keyboard_button',
+        'widget': chrome.HeaderButtonWidget,
+        'append': '.pos-rightheader',
+        'args': {
+            'label': 'Change keyboard led',
+            'action': function () {
+                this.pos.keyboard.action({
+                    'action': 'change_led',
+                });
+            }
+        },
+    },
+
+    build_widgets: function () {
+        if (this.keyboard) {
+            this.widgets.append(this.keyboard_button_widget);
+        }
+    }
+});
+
+});
