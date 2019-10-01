1f72ea75648e13bba22774f2f83bdd591b041397
Mikul <mikulbuncha@Mikuls-MacBook-Pro.local>
2019-10-01 15:01:37 +0200

    Test 2

--- pos_iot_oxp/drivers/IPCameraDriverv2.py
+++ pos_iot_oxp/drivers/IPCameraDriverv2.py
@@ -18,7 +18,8 @@ class IPCameraDriver(Driver):
 
     @classmethod
     def supported(cls, device):
-        return device.driver.decode('utf-8') == 'uvcvideo'
+        # return device.driver.decode('utf-8') == 'uvcvideo'
+        return True
 
     def action(self, data):
         try:
@@ -27,13 +28,22 @@ class IPCameraDriver(Driver):
             Take picture, output it on stdout and convert it in base 64.
             Release Event with picture in data.
             """
-            v4l2 = subprocess.Popen(['v4l2-ctl', '--list-formats-ext'], stdout=subprocess.PIPE)
-            all_sizes = subprocess.Popen(['grep', 'Size'], stdin=v4l2.stdout, stdout=subprocess.PIPE)
-            all_resolutions = subprocess.Popen(['awk', '{print $3}'], stdin=all_sizes.stdout, stdout=subprocess.PIPE)
-            sorted_resolutions = subprocess.Popen(['sort', '-rn'], stdin=all_resolutions.stdout, stdout=subprocess.PIPE)
-            resolution = subprocess.check_output(['awk', 'NR==1'], stdin=sorted_resolutions.stdout).decode('utf-8')
-            self.data['image'] = base64.b64encode(subprocess.check_output(["fswebcam", "-d", self.dev.interface, "-", "-r", resolution]))
-            self.data['message'] = 'Image captured'
+            # v4l2 = subprocess.Popen(['v4l2-ctl', '--list-formats-ext'], stdout=subprocess.PIPE)
+            # all_sizes = subprocess.Popen(['grep', 'Size'], stdin=v4l2.stdout, stdout=subprocess.PIPE)
+            # all_resolutions = subprocess.Popen(['awk', '{print $3}'], stdin=all_sizes.stdout, stdout=subprocess.PIPE)
+            # sorted_resolutions = subprocess.Popen(['sort', '-rn'], stdin=all_resolutions.stdout, stdout=subprocess.PIPE)
+            # resolution = subprocess.check_output(['awk', 'NR==1'], stdin=sorted_resolutions.stdout).decode('utf-8')
+            # self.data['image'] = base64.b64encode(subprocess.check_output(["fswebcam", "-d", self.dev.interface, "-", "-r", resolution]))
+            # self.data['message'] = 'Image captured'
+
+            session = request.Session()
+            session.auth = ('admin','admin')
+            content = session.get('http://192.168.2.27/tmpfs/auto.jpg?111111111').content
+            encodeBytes = base64.b64encode(content)
+            self.data['image'] = encodeBytes
+            self.data['message'] = 'captured'
+
+
         except subprocess.CalledProcessError as e:
             self.data['message'] = e.output
         event_manager.device_changed(self)
