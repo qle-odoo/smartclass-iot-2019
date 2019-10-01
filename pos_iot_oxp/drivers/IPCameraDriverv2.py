c016e81cc6f681f5f09a2512b7b24dd0dc9c3038
Mikul <mikulbuncha@Mikuls-MacBook-Pro.local>
2019-10-01 15:17:04 +0200

    Test 5

--- pos_iot_oxp/drivers/IPCameraDriverv2.py
+++ pos_iot_oxp/drivers/IPCameraDriverv2.py
@@ -3,48 +3,89 @@
 import base64
 import tempfile
 import subprocess
+import requests
+
+# from odoo.addons.hw_drivers.controllers.driver import event_manager, Driver
+
+
+# class IPCameraDriver(Driver):
+#     connection_type = 'cameraIP'
+
+#     def __init__(self, device):
+#         super(CameraDriver, self).__init__(device)
+#         self._device_type = 'camera'
+#         self._device_connection = 'network'
+#         self._device_name =  'HOSAFE' #self.dev.card.decode('utf-8')
+#         self._device_identifier =  'H2MD6PA'
+
+#     @classmethod
+#     def supported(cls, device):
+#         # return device.driver.decode('utf-8') == 'uvcvideo'
+#         return True
+
+#     def action(self, data):
+#         try:
+#             """
+#             Check the max resolution for webcam.
+#             Take picture, output it on stdout and convert it in base 64.
+#             Release Event with picture in data.
+#             """
+#             # v4l2 = subprocess.Popen(['v4l2-ctl', '--list-formats-ext'], stdout=subprocess.PIPE)
+#             # all_sizes = subprocess.Popen(['grep', 'Size'], stdin=v4l2.stdout, stdout=subprocess.PIPE)
+#             # all_resolutions = subprocess.Popen(['awk', '{print $3}'], stdin=all_sizes.stdout, stdout=subprocess.PIPE)
+#             # sorted_resolutions = subprocess.Popen(['sort', '-rn'], stdin=all_resolutions.stdout, stdout=subprocess.PIPE)
+#             # resolution = subprocess.check_output(['awk', 'NR==1'], stdin=sorted_resolutions.stdout).decode('utf-8')
+#             # self.data['image'] = base64.b64encode(subprocess.check_output(["fswebcam", "-d", self.dev.interface, "-", "-r", resolution]))
+#             # self.data['message'] = 'Image captured'
+
+#             session = request.Session()
+#             session.auth = ('admin','admin')
+#             content = session.get('http://192.168.2.27/tmpfs/auto.jpg?111111111').content
+#             encodeBytes = base64.b64encode(content)
+#             self.data['image'] = encodeBytes
+#             self.data['message'] = 'captured'
+
+
+#         except subprocess.CalledProcessError as e:
+#             self.data['message'] = e.output
+#         event_manager.device_changed(self)
 
 from odoo.addons.hw_drivers.controllers.driver import event_manager, Driver
 
 
-class IPCameraDriver(Driver):
+class IPCamDriver(Driver):
     connection_type = 'cameraIP'
 
     def __init__(self, device):
-        super(CameraDriver, self).__init__(device)
+        super(IPCamDriver, self).__init__(device)
         self._device_type = 'camera'
         self._device_connection = 'network'
-        self._device_name =  'HOSAFE' #self.dev.card.decode('utf-8')
-        self._device_identifier =  'H2MD6PA'
+        self._device_name = "HOSAFE"
+        self._device_identifier = "H2MD6PA"
 
     @classmethod
     def supported(cls, device):
-        # return device.driver.decode('utf-8') == 'uvcvideo'
         return True
+
+    @property
+    def device_identifier(self):
+        return self.dev['identifier']
 
     def action(self, data):
         try:
             """
-            Check the max resolution for webcam.
-            Take picture, output it on stdout and convert it in base 64.
+            Take picture and save it to a tmp file.
+            Convert this picture in base 64.
             Release Event with picture in data.
             """
-            # v4l2 = subprocess.Popen(['v4l2-ctl', '--list-formats-ext'], stdout=subprocess.PIPE)
-            # all_sizes = subprocess.Popen(['grep', 'Size'], stdin=v4l2.stdout, stdout=subprocess.PIPE)
-            # all_resolutions = subprocess.Popen(['awk', '{print $3}'], stdin=all_sizes.stdout, stdout=subprocess.PIPE)
-            # sorted_resolutions = subprocess.Popen(['sort', '-rn'], stdin=all_resolutions.stdout, stdout=subprocess.PIPE)
-            # resolution = subprocess.check_output(['awk', 'NR==1'], stdin=sorted_resolutions.stdout).decode('utf-8')
-            # self.data['image'] = base64.b64encode(subprocess.check_output(["fswebcam", "-d", self.dev.interface, "-", "-r", resolution]))
-            # self.data['message'] = 'Image captured'
 
-            session = request.Session()
-            session.auth = ('admin','admin')
-            content = session.get('http://192.168.2.27/tmpfs/auto.jpg?111111111').content
-            encodeBytes = base64.b64encode(content)
-            self.data['image'] = encodeBytes
-            self.data['message'] = 'captured'
-
-
+            session = requests.Session()
+            session.auth = ('admin', 'admin')
+            content = session.get('http://192.168.2.27/tmpfs/auto.jpg?11111111111').content
+            encodedBytes = base64.b64encode(content)
+            self.data['image'] = encodedBytes
+            self.data['message'] = 'Image captured'
         except subprocess.CalledProcessError as e:
             self.data['message'] = e.output
-        event_manager.device_changed(self)
+
+        event_manager.device_changed(self)        
