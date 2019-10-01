cdb21603b922596ae2dfece606ab62b126d42ddc
gab-odoo <gab@odoo.com>
2019-10-01 09:27:43 +0200

    [IMP] pos_iot_oxp: add LED button

---
+++ pos_iot_oxp/static/src/js/chrome.js
@@ -0,0 +1,30 @@
+odoo.define('pos_iot_oxp.chrome', function (require) {
+"use.strict";
+
+var chrome = require('point_of_sale.chrome');
+
+chrome.Chrome.include({
+    keyboard_button_widget: {
+        'name': 'keyboard_button',
+        'widget': chrome.HeaderButtonWidget,
+        'append': '.pos-rightheader',
+        'args':{
+            'label': 'Change Keyboard LED',
+            'action': function () {
+                this.pos.keyboard.action({
+                    'action': 'change_led',
+                });
+            }
+        },
+    },
+
+    build_widgets: function () {
+        if (this.pos.keyboard) {
+            this.widgets.append(this.keyboard_button_widget);
+        }
+        this._super();
+    }
+
+});
+
+});
