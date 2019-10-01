8723525b169adb1f69d26fe5a337dfcda1a0cc1f
Harshad Modi <hmo@odoo.com>
2019-10-01 08:56:21 +0200

    [WIP] pos_iot_oxp

---
+++ pos_iot_oxp/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'pos_iot_oxp',
+    'version': '1.0',
+    'category': 'point of sale',
+    'sequence': 6,
+     'summary': '',
+     'data': ['views/pos_config_view.xml', 'views/point_of_sale_assets.xml'],
+     'depends': ['pos_iot'],
+     'installable': True,
+}
---
+++ pos_iot_oxp/models/pos_config.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+class PoSConfig(models.Model):
+    _inherit = 'pos.config'
+
+    iot_keyboard_id = fields.Many2one(
+               'iot.device',
+               string='Keyboard',
+               domain=[('type','=','keyboard')]
+    )
