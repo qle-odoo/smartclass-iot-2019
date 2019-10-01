b17ddbcb5ce7eb0e24855e0bd0a7a684f4bb77b2
Jigar A. (Odoo) <jam@odoo.com>
2019-10-01 06:30:38 -0700

    [ADD] IPCAM

---
+++ iot_oxp_ipcam/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'IoT OXP Exercise',
+
+    'summary': '''Custom IoT App''',
+
+    'description': '''Custom IoT App''',
+    'author': 'Odoo, Inc',
+    'website': 'https://www.odoo.com',
+    'category': 'IoT',
+    'version': '0.1',
+    'depends': [
+        'pos_iot'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
