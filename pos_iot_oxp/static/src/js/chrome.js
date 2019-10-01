69e2bad2b0716b8d61070c8edb5a3111751a8810
andrewwells92 <root@Andrews-MacBook-Pro-2.local>
2019-10-01 10:28:11 +0200

    LED2.3.2

--- pos_iot_oxp/static/src/js/chrome.js
+++ pos_iot_oxp/static/src/js/chrome.js
@@ -4,6 +4,30 @@ odoo.define('pos_iot_oxp.chrome', function (require) {
     var chrome = require('point_of_sale.chrome');
     
     chrome.Chrome.include({
+        keyboard_light_button_widget: {
+            'name': 'keyboard_light_button',
+            'widget': chrome.HeaderButtonWidget,
+            'append': '.pos-rightheader',
+            'args': {
+                'label': 'Change All Keyboard LEDs',
+                'action': function () {
+                    this.pos.keyboard.action({
+                        'action': 'change_all_led',
+                    });
+                }
+            },
+        },
+    
+        build_widgets: function () {
+            if (this.pos.keyboard) {
+                this.widgets.push(this.keyboard_light_button_widget);
+            }
+            this._super();
+        }
+    });
+
+
+    chrome.Chrome.include({
         keyboard_button_widget: {
             'name': 'keyboard_button',
             'widget': chrome.HeaderButtonWidget,
@@ -26,27 +50,6 @@ odoo.define('pos_iot_oxp.chrome', function (require) {
         }
     });
 
-    chrome.Chrome.include({
-        keyboard_light_button_widget: {
-            'name': 'keyboard_light_button',
-            'widget': chrome.HeaderButtonWidget,
-            'append': '.pos-rightheader',
-            'args': {
-                'label': 'Change All Keyboard LEDs',
-                'action': function () {
-                    this.pos.keyboard.action({
-                        'action': 'change_all_led',
-                    });
-                }
-            },
-        },
-    
-        build_widgets: function () {
-            if (this.pos.keyboard) {
-                this.widgets.push(this.keyboard_light_button_widget);
-            }
-            this._super();
-        }
-    });
+   
     
     });
